678b317a6d5f969293861c5b895cc64b3e7f09ac
benkhalife <nvioir@gmail.com>
2025-10-27 14:29:56 +0000

    feat: add duration to video

---
+++ assets/sidebar-CP6a663h.js
@@ -0,0 +1,1 @@
+import{H as e}from"./index-BoOvDrZ7.js";const s=e("sidebar",{state:()=>({title:""}),actions:{setTitle(t){this.title=t}},getters:{getTitle:t=>t.title}});export{s as u};
--- assets/sidebar-DPieIWI8.js
+++
@@ -1,1 +0,0 @@
-import{H as e}from"./index-o8UsFN38.js";const s=e("sidebar",{state:()=>({title:""}),actions:{setTitle(t){this.title=t}},getters:{getTitle:t=>t.title}});export{s as u};
